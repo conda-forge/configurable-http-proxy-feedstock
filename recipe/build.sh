1aeefdf9e2fac7ca4897f90e18bda7505f4483e2
rxm7706 <95496360+rxm7706@users.noreply.github.com>
2023-08-21 16:59:56 -0500

    Add yarn Install

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,8 +11,12 @@ export npm_config_build_from_source=true
 rm $PREFIX/bin/node
 ln -s $BUILD_PREFIX/bin/node $PREFIX/bin/node
 
+yarn install
 yarn pack
-yarn licenses generate-disclaimer > ThirdPartyLicenses.txt
+yarn licenses generate-disclaimer --production > ThirdPartyLicenses.txt
+
 NPM_CONFIG_USERCONFIG=/tmp/nonexistentrc
 
 npm install -g ${PKG_NAME}-v${PKG_VERSION}.tgz
+
+# npm install --build-from-source -g ${PKG_NAME}-v${PKG_VERSION}.tgz
